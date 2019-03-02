9651f069ee8c5346e69512416284aa70d8d2316b
ShareButter <sharebutterfyp@gmail.com>
2019-03-02 17:03:02 +0800

    [login] reset password design done

--- app/src/main/java/com/fivenine/sharebutter/ForgetPasswordActivity.java
+++ app/src/main/java/com/fivenine/sharebutter/ForgetPasswordActivity.java
@@ -1,16 +1,13 @@
 package com.fivenine.sharebutter;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_forget_password);
     }
 }
