52807254b0a74c6380bd1640adfa8e6476a345c6
ShareButter <sharebutterfyp@gmail.com>
2019-03-05 19:20:32 +0800

    [Home] Home fragment created

---
+++ app/src/main/java/com/fivenine/sharebutter/Home/AuthenticateFragment.java
@@ -0,0 +1,24 @@
+package com.fivenine.sharebutter.Home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fivenine.sharebutter.R;
+
+public class AuthenticateFragment extends Fragment {
+
+    private static final String TAG = "AuthenticateFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_authenticate, container, false);
+
+        return view;
+    }
+
+}
