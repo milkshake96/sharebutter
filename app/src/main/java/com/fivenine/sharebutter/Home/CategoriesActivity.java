c9d0d218dc4e587405e62585dfae262ce64068a6
ShareButter <sharebutterfyp@gmail.com>
2019-03-28 16:30:11 +0800

    [categories] design xml completed

---
+++ app/src/main/java/com/fivenine/sharebutter/Home/CategoriesActivity.java
@@ -0,0 +1,15 @@
+package com.fivenine.sharebutter.Home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fivenine.sharebutter.R;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
