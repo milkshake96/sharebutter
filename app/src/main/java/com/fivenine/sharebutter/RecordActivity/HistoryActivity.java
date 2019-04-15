e19c590f682f02ae02a719f33c5d86c04829f233
ShareButter <sharebutterfyp@gmail.com>
2019-04-15 19:18:27 +0800

    [history] xml done

---
+++ app/src/main/java/com/fivenine/sharebutter/RecordActivity/HistoryActivity.java
@@ -0,0 +1,17 @@
+package com.fivenine.sharebutter.RecordActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fivenine.sharebutter.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+
+    }
+}
